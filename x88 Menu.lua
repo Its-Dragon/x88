2dc09645dc5374c2996c3e0eda197836ac5f2c5b
Its-Dragon <47199787+Its-Dragon@users.noreply.github.com>
2022-08-12 21:25:50 -0500

    Fixed all current features

--- x88 Menu.lua
+++ x88 Menu.lua
@@ -1,6 +1,6 @@
 --[[
 
-    Project: x88 Menu
+    Project: x88Cheats Menu
     Author: Dragon
     LUA API: Primordial
 
@@ -8,48 +8,51 @@
 
 local font = render.create_font("Tahoma", 14, 700);
 
-local real = nil
-local lby = nil
+local local_player = entity_list.get_local_player();
+local player_index = entity_list.get_local_player():get_index()
+
 local kills = nil
 local deaths = nil
-local kd = nil
 local ping = nil
+local kd = nil
 
 local function create_move(cmd)
     local local_player = entity_list.get_local_player();
 
     if engine.is_in_game() then
-        real = engine.get_view_angles();
+        real = engine.get_view_angles(x, y, z);
         lby = local_player:get_prop("m_flLowerBodyYawTarget");
     end
 end
 
 local function welcome()
-    local local_player = entity_list.get_local_player();
-
-    if kills == nil or deaths == nil or ping == nil then
-        kills, deaths, ping, kd = "0", "0", "0", "0";
-    elseif engine.is_in_game() then
-        kills = local_player:get_prop("m_iKills");
-        deaths = local_player:get_prop("m_iDeaths");
-        ping = local_player:get_prop("m_iPing");
+    if engine.is_in_game() then
+        kills = player_resource.get_prop("m_iKills", player_index);
+        deaths = player_resource.get_prop("m_iDeaths", player_index);
+        ping = player_resource.get_prop("m_iPing", player_index);
         kd = kills / deaths;
     end
-    
+
+    if kills == nil or deaths == nil or kd == nil or ping == nil then
+        kills = 0
+        deaths = 0
+        kd = 0
+        ping = 0
+    end
+
     render.text(font, "x88Cheats", vec2_t(5, 35), color_t(192, 108, 129));
     render.text(font, "Hello " .. user.name .. " :)", vec2_t(300, 7), color_t(252, 211, 3));
 
-    render.text(font, "Real: " .. math.floor(real * 100) / 100, vec2_t(300, 37), color_t(255, 255, 255));
+    render.text(font, "Real: " .. math.floor(real.y * 100) / 100, vec2_t(300, 37), color_t(255, 255, 255));
     render.text(font, "LBY: " .. math.floor(lby * 100) / 100, vec2_t(410, 37), color_t(255, 255, 255));
-    render.text(font, "Diff: " .. math.floor((real - lby) * 100) / 100, vec2_t(500, 37), color_t(255, 255, 255));
+    render.text(font, "Diff: " .. math.floor((real.y - lby) * 100) / 100, vec2_t(500, 37), color_t(255, 255, 255));
     render.text(font, "Kills: " .. kills, vec2_t(500, 150), color_t(255, 255, 255));
     render.text(font, "Deaths: " .. deaths, vec2_t(520, 165), color_t(255, 255, 255));
-    render.text(font, "KD: " .. math.floor(kd * 100) / 100, vec2_t(500, 180), color_t(85 * kd, 100 * kd, 0 * kd));
+    render.text(font, "KD: " .. math.floor(kd * 100) / 100, vec2_t(500, 180), color_t(255, 255, 255));
     render.text(font, "Ping: " .. ping, vec2_t(500, 195), color_t(255, 255, 255))
 end
 
 local function main()
-    -- Going to make this all automated var grabbing when I actually can test myself :)
     welcome()
 end
 
